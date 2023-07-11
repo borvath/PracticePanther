19be362f5bec8fd20d39a71d7ee41d4894832d3c
Ben Horvath <borvathcs@gmail.com>
2023-07-11 19:40:02 -0400

    FIXED PROJECT CLOSING BUG!!! this took so long

--- PracticePanther.Library/Models/Project.cs
+++ PracticePanther.Library/Models/Project.cs
@@ -1,36 +1,56 @@
 ﻿using System;
-using PracticePanther.Library.Services;
-
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 namespace PracticePanther.Library.Models;
 
-public class Project {
+public class Project : INotifyPropertyChanged {
+	private DateTime? _open;
+	private DateTime? _close;
+	private bool _isActive;
+	private string _longName;
+	private string? _shortName;
 	
 	public int Id { get; set; }
 	public int ClientId { get; set; }
-	public DateTime? Open { get; set; }
-	public DateTime? Close { get; set; }
-	public bool IsActive { get; set; }
-	public string LongName { get; set; }
-	public string? ShortName { get; set; }
+	public DateTime? Open {
+		get => _open;
+		set { _open = value; NotifyPropertyChanged(); }
+	}
+	public DateTime? Close {
+		get => _close;
+		set { _close = value; NotifyPropertyChanged(); }
+	}
+	public bool IsActive {
+		get => _isActive;
+		set { _isActive = value; NotifyPropertyChanged(); }
+	}
+	public string LongName {
+		get => _longName;
+		set { _longName = value; NotifyPropertyChanged(); }
+	}
+	public string? ShortName { 
+		get => _shortName;
+		set { _shortName = value; NotifyPropertyChanged(); }
+	}
 	public string AsString => ToString();
-	public string AsShortString => ShortToString();
+	public string AsShortString => ToShortString();
 
 	public Project() {
 		Id = 0;
 		Open = DateTime.Today;
 		Close = DateTime.Today.AddYears(1);
 		IsActive = true;
-		LongName = "Default Project";
+		_longName = "Default Project";
 		ShortName = "Project";
 	}
 	public Project(DateTime? open, DateTime? close, string longName, string? shortName) {
 		Open = open;
 		Close = close;
 		IsActive = true;
-		LongName = longName;
+		_longName = longName;
 		ShortName = shortName;
 	}
-	public string ShortToString() {
+	public string ToShortString() {
 		string projIdString = "ID: {0, -5}";
 		string longNameString = "Name: {1, -" + (LongName.Length + 5) + "}";
 		string isActiveString = "Active: {2}";
@@ -44,5 +64,9 @@ public class Project {
 		string isActiveString = "Active: {3}";
 		string ret = projIdString + clientIdString + longNameString + isActiveString;
 		return String.Format(ret, Id, ClientId, LongName, IsActive);
+	}
+	public event PropertyChangedEventHandler? PropertyChanged;
+	protected virtual void NotifyPropertyChanged([CallerMemberName] string? propertyName = null) {
+		PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
 	}
 }
--- PracticePanther.Maui/ViewModels/ClientDisplayViewModel.cs
+++ PracticePanther.Maui/ViewModels/ClientDisplayViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Runtime.CompilerServices;
@@ -12,41 +13,40 @@ namespace PracticePanther.Maui.ViewModels;
 public class ClientDisplayViewModel : IQueryAttributable, INotifyPropertyChanged {
 	
 	public Client? DisplayedClient { get; private set; }
-	public List<Project>? Projects { get; set; }
+	public ObservableCollection<Project>? Projects { get; set; }
 	public Project? SelectedProject { get; set; }
-
 	public bool Closable => AllProjectsClosed();
 
 	public void ApplyQueryAttributes(IDictionary<string, object> query) {
 		Int32.TryParse((query["ClientId"] as string), out int clientId);
 		DisplayedClient = ClientService.Current.Clients.FirstOrDefault(c => c.Id == clientId);
 		if (DisplayedClient != null) {
-			Projects = new List<Project>(ProjectService.Current.Projects.Where(p => p.ClientId == DisplayedClient.Id));
+			Projects = new ObservableCollection<Project>(ProjectService.Current.Projects.Where(p => p.ClientId == DisplayedClient.Id));
 		}
 		NotifyPropertyChanged(nameof(DisplayedClient));
+		NotifyPropertyChanged(nameof(Projects));
 	}
 	public void CloseProject() {
 		if (SelectedProject != null) {
-			SelectedProject.IsActive = false;
 			SelectedProject.Close = DateTime.Now;
-			RefreshView();
+			SelectedProject.IsActive = false;
 		}
+		NotifyPropertyChanged(nameof(Projects));
+		NotifyPropertyChanged(nameof(Closable));
 	}
 	public void CloseClient() {
 		if (DisplayedClient != null) {
 			DisplayedClient.IsActive = false;
 			DisplayedClient.Close = DateTime.Now;
-			NotifyPropertyChanged(nameof(DisplayedClient));
 		}
+		NotifyPropertyChanged(nameof(DisplayedClient));
 	}
 	private bool AllProjectsClosed() {
-		return ProjectService.Current.Projects.Where(p => p.ClientId == DisplayedClient?.Id && !p.IsActive).ToList().Count == 0;
+		return ProjectService.Current.Projects.Where(p => p.ClientId == DisplayedClient?.Id && p.IsActive).ToList().Count == 0;
 	}
 	public void RefreshView() {
 		NotifyPropertyChanged(nameof(DisplayedClient));
-		if (DisplayedClient != null) {
-			Projects = new List<Project>(ProjectService.Current.Projects.Where(p => p.ClientId == DisplayedClient.Id));
-		}
+		NotifyPropertyChanged(nameof(SelectedProject));
 		NotifyPropertyChanged(nameof(Projects));
 		NotifyPropertyChanged(nameof(Closable));
 	}
