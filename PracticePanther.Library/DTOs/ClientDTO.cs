8ee5b78538d37188aa99f4d89a9bd99ed0562279
Ben Horvath <borvathcs@gmail.com>
2023-07-30 01:51:32 -0400

    Added some overlooked files from the API addition

---
+++ PracticePanther.Library/DTOs/ClientDTO.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using PracticePanther.Library.Models;
+
+namespace PracticePanther.Library.DTOs; 
+
+public class ClientDTO {
+	public int Id { get; }
+	public string Name { get; }
+	public DateTime Open { get; }
+	public DateTime? Close { get; }
+	public bool Active { get; }
+	public string? Notes { get; }
+	
+	public ClientDTO(int id, string name, DateTime open, DateTime? close, string? notes, bool active) {
+		Id = id;
+		Name = name;
+		Open = open;
+		Close = close;
+		Notes = notes;
+		Active = active;
+	}
+	public Client ConvertToClient() {
+		return new Client(Id, Name, Open, Close, Notes, Active);
+	}
+}
---
+++ PracticePanther.Library/Utilities/WebRequestHandler.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace PracticePanther.Library.Utilities;
+
+public class WebRequestHandler {
+	private const string host = "localhost";
+	private const string port = "7216";
+	private HttpClient Client { get; }
+    
+	public WebRequestHandler() {
+		Client = new HttpClient();
+	}
+	public async Task<string?> Get(string url) {
+		try {
+			return await Client.GetStringAsync($"https://{host}:{port}{url}").ConfigureAwait(false);
+		}
+		catch (Exception) { } return null;
+	}
+	public async Task<string?> Delete(string url) {
+		try {
+			using var request = new HttpRequestMessage(HttpMethod.Delete, $"https://{host}:{port}{url}");
+			using HttpResponseMessage response = await Client.SendAsync(request, HttpCompletionOption.ResponseHeadersRead).ConfigureAwait(false);
+			return (response.IsSuccessStatusCode) ? await response.Content.ReadAsStringAsync() : "ERROR";
+		}
+		catch (Exception) { } return null;
+	}
+	public async Task<string> Post(string url, object obj)
+	{
+		using var request = new HttpRequestMessage(HttpMethod.Post, $"https://{host}:{port}{url}");
+		request.Content = new StringContent(JsonConvert.SerializeObject(obj), Encoding.UTF8, "application/json");
+		using HttpResponseMessage response = await Client.SendAsync(request, HttpCompletionOption.ResponseHeadersRead).ConfigureAwait(false);
+		return (response.IsSuccessStatusCode) ? await response.Content.ReadAsStringAsync() : "ERROR";
+	}
+}
